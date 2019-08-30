f9042a3578b80fb7a9816356a7693693c1cbbd2c
tom licha <tomylicha@hotmail.fr>
2019-08-30 11:00:21 +0200

    fix vulnerability

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import * as firebase from 'firebase';
 export class AppComponent {
   constructor() {
     const config = {
-      apiKey: "AIzaSyCjpuX2maM-eCxztDdE2F28NYp6sSBIRp4",
+      apiKey: "youAPIKEY",
       authDomain: "bookshelves-oc-980dd.firebaseapp.com",
       databaseURL: "https://bookshelves-oc-980dd.firebaseio.com",
       projectId: "bookshelves-oc-980dd",
