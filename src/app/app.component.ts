269adcbc42d1b9c83b2a637124b8172e1be2a57a
tom licha <tomylicha@hotmail.fr>
2019-08-30 10:43:52 +0200

    adding firebase storage and auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bookshelves';
+  constructor() {
+    const config = {
+      apiKey: "AIzaSyCjpuX2maM-eCxztDdE2F28NYp6sSBIRp4",
+      authDomain: "bookshelves-oc-980dd.firebaseapp.com",
+      databaseURL: "https://bookshelves-oc-980dd.firebaseio.com",
+      projectId: "bookshelves-oc-980dd",
+      storageBucket: "gs://bookshelves-oc-980dd.appspot.com/",
+      messagingSenderId: "109935920454",
+      appId: "1:109935920454:web:aaf6c8d6a334c210"
+    };
+    firebase.initializeApp(config);
+  }
 }
